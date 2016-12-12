862a5bd69fb274353e184a73790f361e709c1368
pwg96 <pwg96pl@gmail.com>
2016-12-12 02:39:04 +0100

    mpc-hc np: added new output string

--- weechat-np-mpc-hc.py
+++ weechat-np-mpc-hc.py
@@ -5,12 +5,13 @@ import weechat
 import urllib2 
 import re 
 
-#def get_mpc_hc_info():
-#   player = urllib2.urlopen("http://pwg.space/mpc.html").read()
-#   info = re.search('&bull;\s(.*)\s&bull;\s(.*)\s&bull;', player)
-#   filename = info.group(1)
-#   progress = info.group(2)
-#  return filename, progress
+def get_mpc_hc_info():
+   player = urllib2.urlopen("http://pwg.space/mpc.html").read()
+   info = re.search('&laquo;\s(.*)\s&bull;\s(.*)\s&bull;\s(.*)\s&bull;', player)
+   filename = info.group(2)
+   progress = info.group(3)
+   version = info.group(1)
+   return filename, progress, version
 
 def get_mpc_hc_status():
    player = urllib2.urlopen("http://pwg.space/mpc_status.html").read()
@@ -32,7 +33,14 @@ def mpc_hc_np(data, buffer, args):
       "/me is watching \x02{} {}/{} {} ({}%)".format(filename, pos_str, dur_str, bar, percent))
    return weechat.WEECHAT_RC_OK
 
+def mpc_hc_info(data, buffer, args):
+   filename, progress, version = get_mpc_hc_info()
+   weechat.command(weechat.current_buffer(),
+      "/me is watching \x02{} \x0Fat \x02{} \x0Fvia \x02{}".format(filename, progress, version))
+   return weechat.WEECHAT_RC_OK
+   
 weechat.register("weechat-np-mpv", "Bodzioslaw aka pwg", "0.1", "LGPL", "Simple mow playing for mpc_hc", "", "")
 
 hook = weechat.hook_command("mpc", "Now playing mpc-hc", "", "", "", "mpc_hc_np", "")
 
+hook = weechat.hook_command("mpc-info", "mpc-hc info", "", "", "", "mpc_hc_info", "")
