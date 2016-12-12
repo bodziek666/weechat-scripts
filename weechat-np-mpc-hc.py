7928430f1e0b11b7e7d21683c63fbb7bf5971ea1
xxx yyy <filo1986@gmail.com>
2016-12-13 00:35:24 +0100

    Small rewrite

--- weechat-np-mpc-hc.py
+++ weechat-np-mpc-hc.py
@@ -8,19 +8,21 @@ import re
 def get_mpc_hc_info():
    player = urllib2.urlopen("http://127.0.0.1:13579/info.html").read()
    info = re.search('&laquo;\s(.*)\s&bull;\s(.*)\s&bull;\s(.*)\s&bull;', player)
-   filename = info.group(2)
-   progress = info.group(3)
-   version = info.group(1)
+   version, filename, progress = info.groups()
    return filename, progress, version
 
 def get_mpc_hc_status():
    player = urllib2.urlopen("http://127.0.0.1:13579/status.html").read()
    status = re.search('\("(.*?)",\s"(.*?)",\s(\d*),\s"(.*?)",\s(\d*),\s"(.*?)",', player)
-   filename = status.group(1)
-   pos = int(status.group(3))
-   dur = int(status.group(5))
-   pos_str = status.group(4)
-   dur_str = status.group(6)
+   filename, *, pos, dur, pos_str, dur_str = status.groups()
+   pos = int(pos) #
+   dur = int(dur) # gotta rewrite those two lines into something clever...
+
+   #filename = status.group(1)
+   #pos = int(status.group(3))
+   #dur = int(status.group(5))
+   #pos_str = status.group(4)
+   #dur_str = status.group(6)
    percent = int((pos/dur) * 100)
    bar_prog = int(round((pos/dur)*15, 1))
    bar = '['+'>'*bar_prog+'-'*(15-bar_prog)+']'
