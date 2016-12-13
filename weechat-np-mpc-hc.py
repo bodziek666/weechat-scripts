0e8081690613c7ec0afab70079347d3b6bbdd88a
pwg96 <pwg96pl@gmail.com>
2016-12-13 01:57:01 +0100

    Shorten version of the script

--- weechat-np-mpc-hc.py
+++ weechat-np-mpc-hc.py
@@ -8,20 +8,23 @@ import re
 def get_mpc_hc_info():
    player = urllib2.urlopen("http://127.0.0.1:13579/info.html").read()
    info = re.search('&laquo;\s(.*)\s&bull;\s(.*)\s&bull;\s(.*)\s&bull;', player)
-   filename = info.group(2)
-   progress = info.group(3)
-   version = info.group(1)
+#   filename = info.group(2)
+#   progress = info.group(3)
+#   version = info.group(1)
+   version, filename, progress = info.groups()
    return filename, progress, version
 
 def get_mpc_hc_status():
    player = urllib2.urlopen("http://127.0.0.1:13579/status.html").read()
-   status = re.search('\("(.*?)",\s"(.*?)",\s(\d*),\s"(.*?)",\s(\d*),\s"(.*?)",', player)
-   filename = status.group(1)
-   pos = int(status.group(3))
-   dur = int(status.group(5))
-   pos_str = status.group(4)
-   dur_str = status.group(6)
+   status = re.search('\("(.*?)",\s"(.*?)",\s(?:\d*),\s"(.*?)",\s(\d*),\s"(.*?)",', player)
+#   status = re.search('\("(.*?)",\s"(.*?)",\s(\d*),\s"(.*?)",\s(\d*),\s"(.*?)",', player)
+#   filename = status.group(1)
+#   pos = int(status.group(3))
+#   dur = int(status.group(5))
+#   pos_str = status.group(4)
+#   dur_str = status.group(6)
    percent = int((pos/dur) * 100)
+   filename, pos, pos_str, dur, dur_str = status.groups()
    bar_prog = int(round((pos/dur)*15, 1))
    bar = '['+'>'*bar_prog+'-'*(15-bar_prog)+']'
    return filename, pos_str, dur_str, percent, bar 
