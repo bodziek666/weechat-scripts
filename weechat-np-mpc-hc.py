a081af35852d506ebd4cc221b7d479254cf306de
pwg96 <pwg96pl@gmail.com>
2016-12-13 02:40:40 +0100

    Deleted garbage lines

--- weechat-np-mpc-hc.py
+++ weechat-np-mpc-hc.py
@@ -8,9 +8,6 @@ import re
 def get_mpc_hc_info():
    player = urllib2.urlopen("http://127.0.0.1:13579/info.html").read()
    info = re.search('&laquo;\s(.*)\s&bull;\s(.*)\s&bull;\s(.*)\s&bull;', player)
-#   filename = info.group(2)
-#   progress = info.group(3)
-#   version = info.group(1)
    version, filename, progress = info.groups()
    filename = filename.replace('_', ' ').encode('UTF-8')
    return filename, progress, version
@@ -18,16 +15,7 @@ def get_mpc_hc_info():
 def get_mpc_hc_status():
    player = urllib2.urlopen("http://127.0.0.1:13579/status.html").read()
    status = re.search('\("(.*?)",\s"(.*?)",\s(\d*),\s"(.*?)",\s(\d*),\s"(.*?)",', player)
-#   status = re.search('\("(.*?)",\s"(.*?)",\s(\d*),\s"(.*?)",\s(\d*),\s"(.*?)",', player)
-#   filename = status.group(1)
-#   pos = int(status.group(3))
-#   dur = int(status.group(5))
-#   pos_str = status.group(4)
-#   dur_str = status.group(6)
    filename, _, pos, pos_str, dur, dur_str = status.groups()
-   #percent = int((pos/dur) * 100)
-   #bar_prog = int(round((pos/dur)*15, 1))
-   #bar = '['+'>'*bar_prog+'-'*(15-bar_prog)+']'
    return filename, pos, pos_str, dur, dur_str
 
 def parse_mpc_hc_status():
@@ -52,7 +40,7 @@ def mpc_hc_info(data, buffer, args):
       "/me is watching \x02{} \x0Fat \x02{} \x0Fvia \x02{}".format(filename, progress, version))
    return weechat.WEECHAT_RC_OK
    
-weechat.register("weechat-np-mpc-hc", "Bodzioslaw aka pwg", "0.1", "LGPL", "Simple mow playing for mpc_hc", "", "")
+weechat.register("weechat-np-mpc-hc", "Bodzioslaw aka pwg", "0.2", "LGPL", "Simple mow playing for mpc_hc", "", "")
 
 hook = weechat.hook_command("mpc", "Now playing mpc-hc", "", "", "", "mpc_hc_np", "")
 
