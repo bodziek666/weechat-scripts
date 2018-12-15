6666fd9586b61402750c04b20f0881f12358a899
Tobias Perelstein <tobias.perelstein@gmail.com>
2018-12-15 17:03:27 -0500

    Simplify mpv np script for my purposes

--- weechat-np-mpv.py
+++ weechat-np-mpv.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import weechat 
+import weechat
 import json
 import socket
-import os.path 
+import os.path
+import os
+import fnmatch
 import time
 
+def find_mpv_socket():
+    base_dir = "/tmp"
+    mpv_sock_pattern ='*mpv*.sock'
+    matches = []
+    for root, dirnames, filenames in os.walk(base_dir):
+        for filename in fnmatch.filter(filenames, mpv_sock_pattern):
+            matches.append(os.path.join(root, filename))
+    if not matches or len(matches) > 1:
+        raise Exception
+    return matches.pop()
+
 def get_mpv_info():
-	client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-	client.connect("/tmp/mpvsocket")
-	res = {}
-# 
-# Length property was deprecated after the mpv 0.9 release, use 'duration' instead
-#
-	for i in ['filename', 'media-title', 'playback-time', 'length']:
-		req = '{"command": ["get_property", "%s"]}\n' %i
-		client.send(req)
-		res[i] = json.loads(client.recv(1024))['data']
-		
-	client.close()
-	return res
-	
+    mpv_socket = find_mpv_socket()
+    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    client.connect(mpv_socket)
+    res = {}
+    #
+    # Length property was deprecated after the mpv 0.9 release, use 'duration' instead
+    #
+    for i in ['title']: # TODO add more things later
+        req = '{{"command": ["get_property", "{}"]}}\n'.format(i)
+        client.send(req)
+        res[i] = json.loads(client.recv(1024))['data']
+
+    client.close()
+    return res
+
 def parse_info():
-	np = get_mpv_info()
-	filename = np['filename'].replace('_', ' ').encode('UTF-8')
-	title = np['media-title'].replace('_', ' ').encode('UTF-8')
-	progress = time.strftime("%H:%M:%S",time.gmtime(np['playback-time']))
-	duration = time.strftime("%H:%M:%S", time.gmtime(np['length']))
-	percent = int((np['playback-time']/np['length']) * 100)
-       
-	bar_prog = int(round((np['playback-time']/np['length'])*15, 1))
-	bar = '['+'>'*bar_prog+'-'*(15-bar_prog)+']'
-	return filename, title, progress, duration, percent, bar
- 
+    try:
+        np = get_mpv_info()
+    except:
+        return ("Unknown")
+
+    title = np['title'].replace('_', ' ').encode('UTF-8')
+
+    return title
+
 def mpv_np(data, buffer, args):
-	filename, title, progress, duration, percent, bar = parse_info()
-	weechat.command(weechat.current_buffer(), 
-	    "/me is watching \x02{} {}/{} {} ({}%)".format(filename, progress, duration, bar, percent))                                                   
-	return weechat.WEECHAT_RC_OK
- 
-def mpv_np_title(data, buffer, args):
-	filename, title, progress, duration, percent, bar = parse_info()
-	weechat.command(weechat.current_buffer(), 
-	    "/me is watching \x02{} {}/{} {} ({}%)".format(title, progress, duration, bar, percent))
-	return weechat.WEECHAT_RC_OK  
+    title = parse_info()
+    weechat.command(weechat.current_buffer(),
+            "Now playing: {}".format(title))
+    return weechat.WEECHAT_RC_OK
+
 
 weechat.register("weechat-np-mpv", "Bodzioslaw aka pwg", "0.1", "LGPL", "Now playing for mpv", "", "")
 
 hook = weechat.hook_command("mpv", "Now playing mpv", "", "", "", "mpv_np", "")
-hook = weechat.hook_command("mpv-title", "Now playing mpv with tittle", "", "", "", "mpv_np_title", "")
